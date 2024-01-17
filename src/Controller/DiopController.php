41e60b9bfe23dac270826ef0ab6fae06a1f5e697
Bineta Diop <bineta.diop@etu.univ-st-etienne.fr>
2024-01-17 23:06:33 +0100

    Bineta'website

--- src/Controller/DiopController.php
+++ src/Controller/DiopController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Annotation\Route;     
 
 class DiopController extends AbstractController
 {
@@ -12,6 +12,41 @@ class DiopController extends AbstractController
     public function Diop(): Response
     {
         return $this->render('diop/index.html.twig', [
+            'controller_name' => 'DiopController',
+        ]);
+    }
+    #[Route('/accueil', name: 'accueil')]
+    public function accueil(): Response
+    {
+        return $this->render('diop/accueil.html.twig', [
+            'controller_name' => 'DiopController',
+        ]);
+    }
+    #[Route('/A propos de moi', name: 'A propos de moi')]
+    public function A_propos_de_moi(): Response
+    {
+        return $this->render('diop/A propos de moi.html.twig', [
+            'controller_name' => 'DiopController',
+        ]);
+    }
+    #[Route('/cv', name: 'cv')]
+    public function cv(): Response
+    {
+        return $this->render('diop/cv.html.twig', [
+            'controller_name' => 'DiopController',
+        ]);
+    }
+    #[Route('/portfolio', name: 'portfolio')]
+    public function porfolio(): Response
+    {
+        return $this->render('diop/portfolio.html.twig', [
+            'controller_name' => 'DiopController',
+        ]);
+    }
+    #[Route('/formulaire', name: 'formulaire')]
+    public function formulaire(): Response
+    {
+        return $this->render('diop/formulaire.html.twig', [
             'controller_name' => 'DiopController',
         ]);
     }
